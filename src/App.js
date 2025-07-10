084b0df518efaa9cae5facfee3525c7b4db656d1
𝑱𝒂𝒏 𝑸𝒖𝒆𝒓𝒂𝒍𝒕 <55952925+jaanque@users.noreply.github.com>
2025-07-11 00:41:45 +0200

    UPDATE

--- src/App.js
+++ src/App.js
@@ -6,21 +6,24 @@ function App() {
       <div className="App-content">
         <div className="App-left">
           <img 
+            draggable="false"
             src="https://i.postimg.cc/287qgkGV/image-1.webp" 
             alt="Logo de la aplicación" 
             className="App-logo"
           />
-          <h1 className="App-title">Mi Aplicación</h1>
+          <h1 className="App-title">ParkMi - Encuentra tu plaza</h1>
           <p className="App-subtitle">
-            Descubre una nueva forma de gestionar tus tareas diarias
+            Parkmi te ayuda a encontrar parking rápido, sin complicaciones. Solo tienes que buscar y aparcar donde lo necesites. ¡Así de fácil!
           </p>
+          <br />
           <button className="App-cta">
             Comenzar Ahora
           </button>
         </div>
         
         <div className="App-right">
-          <img 
+          <img
+            draggable="false"
             src="https://i.postimg.cc/R0DGfQn5/20250710-1816-Estacionamiento-Reservado-Digital-remix-01jzthv8thft18ty0h4e2kmp9v.png"
             alt="Imagen de la aplicación" 
             className="App-image"
