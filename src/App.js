98173383631f6a8fbe8b3651e35827b692cfd0f0
𝑱𝒂𝒏 𝑸𝒖𝒆𝒓𝒂𝒍𝒕 <55952925+jaanque@users.noreply.github.com>
2025-07-11 01:08:03 +0200

    UPDATE

--- src/App.js
+++ src/App.js
@@ -16,9 +16,22 @@ function App() {
             Parkmi te ayuda a encontrar parking rápido, sin complicaciones. Solo tienes que buscar y aparcar donde lo necesites. ¡Así de fácil!
           </p>
           <br />
-          <button className="App-cta">
-            Comenzar Ahora
-          </button>
+          <div className="App-buttons">
+            <button className="App-cta">
+              Comenzar Ahora
+            </button>
+            <div className="App-store-section">
+              <div className="App-store-buttons">
+                <a href="#" className="App-store-btn">
+                  <img src="https://upload.wikimedia.org/wikipedia/commons/7/78/Google_Play_Store_badge_EN.svg" alt="Google Play" />
+                </a>
+                <a href="#" className="App-store-btn">
+                  <img src="https://upload.wikimedia.org/wikipedia/commons/3/3c/Download_on_the_App_Store_Badge.svg" alt="App Store" />
+                </a>
+              </div>
+              <p className="App-store-text">(próximamente)</p>
+            </div>
+          </div>
         </div>
         
         <div className="App-right">
