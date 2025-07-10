766cd65a41c56fae9b79e57748598b6a9e01f3cc
𝑱𝒂𝒏 𝑸𝒖𝒆𝒓𝒂𝒍𝒕 <55952925+jaanque@users.noreply.github.com>
2025-07-10 17:18:45 +0200

    RESET

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
 import React from 'react';
 import './App.css';
-import Hero from './components/Hero';
-import IdeaExplanation from './components/IdeaExplanation';
-import ForDrivers from './components/ForDrivers';
-import ForInvestors from './components/ForInvestors';
-import Faq from './components/Faq';
-import RegistrationForm from './components/RegistrationForm';
 
 function App() {
   return (
     <div className="App">
-      <Hero />
-      <main>
-        <IdeaExplanation />
-        <ForDrivers />
-        <ForInvestors />
-        <Faq />
-        <RegistrationForm />
-      </main>
-      {/* Podríamos añadir un Footer global aquí si fuera necesario más adelante */}
-      {/* Ejemplo: <Footer /> */}
+      <h1>Bienvenido a WeCar</h1>
     </div>
   );
 }
