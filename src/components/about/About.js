c39652b65d592c78a8971235c40ce44eae78bcc9
𝑱𝒂𝒏 𝑸𝒖𝒆𝒓𝒂𝒍𝒕 <55952925+jaanque@users.noreply.github.com>
2025-07-11 12:11:31 +0200

    UPDATE // Footer

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -23,6 +23,7 @@ function About() {
           Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
           Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
         </p>
+        <br />
         <button className="App-button">Descobreix Growly</button>
       </div>
     </div>
